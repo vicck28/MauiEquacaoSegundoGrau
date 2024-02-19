59bc680e396f9c7eeb7f9edcbe0481d3d07b82ac
Usuario <Usuario@CPD03-M20>
2024-02-19 11:33:28 -0300

    Adicionar arquivos de projeto.

---
+++ MauiEquacaoSegundoGrau/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MauiEquacaoSegundoGrau
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
